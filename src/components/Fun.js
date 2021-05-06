c1919e8f0763ab1628042c0308adb72da9dc7d41
Amith <amithpasham@gmail.com>
2021-05-06 18:21:47 +0530

    hey

--- src/components/Fun.js
+++ src/components/Fun.js
@@ -1,13 +1,11 @@
 import React, {Component, useState} from "react";
 function Fun() {
-function funn(){
-    var v=document.getElementById("para");
-    v.innerHTML="Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
-}
+  const [count, setCount] = useState("");
     return (
       <div id="main">
-        <p id="para"></p>
-      <button id="click" onClick={funn}>click</button>
+        <p id="para">{count}</p>
+      <button id="click" onClick={() => setCount("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")}>click
+      </button>
       </div>
     );
   }
