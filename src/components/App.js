96ad52d2bc68d9b7b95bf5e599ddeda74a01c08d
Amith <amithpasham@gmail.com>
2021-05-06 18:26:12 +0530

    heyyy

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,16 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
-
 function App() {
-  return (
-    <div id="main">
-     
-      <Fun/>
-    </div>
-  );
-}
+  const [count, setCount] = useState("");
+    return (
+      <div id="main">
+        <p id="para">{count}</p>
+      <button id="click" onClick={() => setCount("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")}>click
+      </button>
+      </div>
+    );
+  }
+
 
 
 export default App;
