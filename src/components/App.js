74655e792a63257714fcb6fa3afacd70b2d98af6
Amith <amithpasham@gmail.com>
2021-05-07 17:21:01 +0530

    heyy

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
+
 function App() {
   const [count, setCount] = useState(false);
     return (
       <div id="main">
-       count==true?<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>:""
+       {count==true?<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>:''}
   
       <button id="click" onClick={() => setCount(true)}>click
       </button>
@@ -14,4 +15,6 @@ function App() {
 
 
 
+
 export default App;
+
